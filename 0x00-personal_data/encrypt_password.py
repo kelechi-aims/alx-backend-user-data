a170f6988f06f296e82a244946e83cc3074fd135
kelechi-aims <kelechi@aims.edu.gh>
2024-02-09 00:56:07 +0100

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" Encrypting passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes the input password using bcrypt with salt"""
+    # Generate a random salt and hash the password
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
