fa98d7bc4295aaae6addc1a8fea49fcba741e6b6
kelechi-aims <kelechi@aims.edu.gh>
2024-02-09 01:02:41 +0100

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ def hash_password(password: str) -> bytes:
     """Hashes the input password using bcrypt with salt"""
     # Generate a random salt and hash the password
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check valid password"""
+    return bcrypt.checkpw(password.encode('utf'), hashed_password)
