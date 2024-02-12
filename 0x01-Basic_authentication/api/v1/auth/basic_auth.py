c12df9fa84294e375bdba75ee84887cb3c3befc7
kelechi-aims <kelechi@aims.edu.gh>
2024-02-12 20:45:57 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Cass BasicAuth that inherits from Auth """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        Extract the Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
