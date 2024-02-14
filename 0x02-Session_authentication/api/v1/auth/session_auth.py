5c176a065726bfe559662794980db75e806c3960
kelechi-aims <kelechi@aims.edu.gh>
2024-02-14 23:39:57 +0100

    Empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,9 +13,15 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a Session ID for a user_id"""
+        """Creates a Session"""
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """User ID for Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
