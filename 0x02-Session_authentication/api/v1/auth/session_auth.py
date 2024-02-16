8433dc7015ccd3a0d95b134307e7ff419b228193
kelechi-aims <kelechi@aims.edu.gh>
2024-02-16 01:37:03 +0100

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,16 @@ class SessionAuth(Auth):
         if user_id is None:
             return None
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Ddeletes the user session logout"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        self.user_id_by_session_id.pop(session_id, None)
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -34,3 +34,13 @@ def session_login():
             return response
         return jsonify({"error": "wrong password"}), 401
     return jsonify({"error": "no user found for this email"}), 404
+
+
+@app_views.route('auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """Logout user"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    jsonify({}), 200
