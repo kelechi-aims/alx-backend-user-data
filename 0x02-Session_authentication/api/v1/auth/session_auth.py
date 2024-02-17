5a148d8b64b3f91dc4fce70966e48f758b0d7af0
kelechi-aims <kelechi@aims.edu.gh>
2024-02-17 19:13:02 +0100

    correction

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -45,10 +45,13 @@ class SessionAuth(Auth):
         if request is None:
             return False
         session_id = self.session_cookie(request)
-        if session_id is None:
+        if not session_id:
             return False
         user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
+        if not user_id:
             return False
-        self.user_id_by_session_id.pop(session_id, None)
-        return True
+        try:
+            del self.user_id_by_session_id[session_id]
+            return True
+        except KeyError:
+            return False
