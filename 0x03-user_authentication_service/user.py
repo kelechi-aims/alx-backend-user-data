3f6abc6e761d510f21b076456f5229a0715b00b8
kelechi-aims <kelechi@aims.edu.gh>
2024-02-20 12:12:40 +0100

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 """Main file for SQLAlchemy model declaration"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-
 
 # Create a base class for declarative class definitions
 Base = declarative_base()
 
 class User(Base):
-    """SQLAlchemy model for the 'users' table"""
-    __table__ = 'users'
+    """
+    SQLAlchemy model for the 'users' table
+
+    Attributes:
+        id (int): Primary key for the user.
+        email (str): Email address of the user.
+        hashed_password (str): Hashed password of the user.
+        session_id (str, optional): Session ID of the user.
+        reset_token (str, optional): Reset token for the user's password reset.
+    """
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
