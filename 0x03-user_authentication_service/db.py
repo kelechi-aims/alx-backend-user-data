15bf684511d540bdba58e9152fc0dda6e1553bb0
kelechi-aims <kelechi@aims.edu.gh>
2024-02-20 12:45:41 +0100

    User model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,6 +40,8 @@ class DB:
         Returns:
             User: User object representing the added user
         """
+        if email is None or hashed_password is None:
+            return None
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
