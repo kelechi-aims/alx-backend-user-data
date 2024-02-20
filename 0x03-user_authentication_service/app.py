ec6c15799fcdd4a55425f58c44dd186b960150b5
kelechi-aims <kelechi@aims.edu.gh>
2024-02-20 19:29:51 +0100

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -45,12 +45,12 @@ def login() -> str:
 def logout() -> str:
     """Endpoint to logout a user."""
     session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id=session_id)
+    user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
-        return jsonify(message='User not found'), 403
+        abort(403)
 
 
 if __name__ == "__main__":
